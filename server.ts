f5a00f1594f103d2e1b7c0fa9d292af4fb57b087
Divan Visagie <visagiedivan@gmail.com>
2020-01-11 15:28:01 +0200

    bring in unleash

--- server.ts
+++ server.ts
@@ -1,4 +1,17 @@
 import express, {Express, Request} from 'express'
+import {initialize, isEnabled} from 'unleash-client'
+
+const instance = initialize({
+    url: 'http://localhost:4242',
+    appName: 'node-unleash-example',
+    instanceId: 'my-server-instance',
+});
+
+instance.on('ready', () => {
+    console.log('unleash is ready')
+})
+instance.on('error', console.error);
+instance.on('warn', console.warn);
 
 const app = express()
 
@@ -13,11 +26,14 @@ const config = {
 }
 
 app.get('/greeting', (req, res) => {
-    if (config.getBool('greet-by-name-feature')) {
+
+    if (isEnabled('greet-by-name-feature')) {
         let user = getUserDetails(req)
         return res.send(`Hello ${user.name}`)
     }
     res.send('Hello World')
+
+    
 })
 
 app.listen(8000)
